8069f8926b0707c736c905d6b0f45de5ab2fe947
黑夜明灯 <952873563@qq.com>
2019-10-28 22:02:56 +0800

    测试：解决找不到上下文

--- src/test/java/cn/edu/fjzzit/web/myhotel/mapper/UserInfoMapperTests.java
+++ src/test/java/cn/edu/fjzzit/web/myhotel/mapper/UserInfoMapperTests.java
@@ -4,12 +4,11 @@ import cn.edu.fjzzit.web.myhotel.model.UserInfo;
 import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.junit.Assert;
-import org.junit.Test;
+
 import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -26,10 +25,12 @@ public class UserInfoMapperTests {
 
     @Test
     public void testInsert(){
+        String userName = "Robin";
         String salt = "abcd";
         UserInfo userInfo = new UserInfo();
          //加密
         SimpleHash simpleHash = new SimpleHash(Md5Hash.ALGORITHM_NAME,"123456",salt,3);
+        userInfo.setUserName(userName);
         userInfo.setSalt(salt);
         userInfo.setPassword(simpleHash.toString());
         userInfo.setCreateTime(new Date());
